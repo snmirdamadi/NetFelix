06bf6a8c756f994cfb1903d0151e71cd1ddde717
notsureme <79614086+notsureme@users.noreply.github.com>
2021-08-26 11:11:07 +0430

    Tests Implemented

--- videos/tests.py
+++ videos/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Video
+
+
+class VideoModelTestCase(TestCase):
+
+    def setUp(self) -> None:
+        Video.objects.create(title='This is my title')
+
+    def test_valid_title(self):
+        title = 'This is my title'
+        qs = Video.objects.filter(title=title)
+        self.assertTrue(qs.exists())
+
