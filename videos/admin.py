820d25e0e500218f9b154f164c68b9718a8de783
notsureme <79614086+notsureme@users.noreply.github.com>
2021-08-26 12:19:48 +0430

    Video Proxies and Admin Panel Fields is settled

--- videos/admin.py
+++ videos/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
 
-from .models import Video
+from .models import VideoAllProxy, VideoPublishedProxy
 
-admin.site.register(Video)
+
+class VideoAllAdmin(admin.ModelAdmin):
+    list_display = ['title', 'id', 'video_id', 'is_published']
+    search_fields = ['title']
+    list_filter = ['active']
+    readonly_fields = ['id', 'is_published']
+    class Meta:
+        model = VideoAllProxy
+
+
+
+
+admin.site.register(VideoAllProxy, VideoAllAdmin)
+
+
+class VideoProxyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id']
+    search_fields = ['title']
+
+    # list_filter = ['video_id']
+    class Meta:
+        model = VideoPublishedProxy
+
+    def get_queryset(self, request):
+        return VideoPublishedProxy.objects.filter(active=True)
+
+
+admin.site.register(VideoPublishedProxy, VideoProxyAdmin)
--- videos/models.py
+++ videos/models.py
@@ -7,13 +7,26 @@ class Video(models.Model):
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
     video_id = models.CharField(max_length=220)
+    active = models.BooleanField(default=True)
+
     # timestamp
     # updated
     # state
     # publish_timestampe
+    
+    @property
+    def is_published(self):
+        return self.active
 
 
-class VideoProxy(Video):
+class VideoAllProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'All Video'
+        verbose_name_plural = 'All Videos'
+
+
+class VideoPublishedProxy(Video):
     class Meta:
         proxy = True
         verbose_name = 'Published Video'
